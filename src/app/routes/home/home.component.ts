ff582c2ea0ca99ccdd03dfe78daf8d95c150cf1c
tjjevans1 <t.evans@catchdigital.com>
2018-04-04 10:31:03 +0100

    Track mouse position

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, ElementRef, AfterViewInit } from '@angular/core';
+
+class coords {
+  x: number;
+  y: number;
+};
 
 @Component({
   selector: 'ph-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  mousePos: coords = new coords;
+  svgPos: coords = new coords;
+
+  @ViewChildren('letter') letters: ElementRef[];
+  @ViewChild('svgElem') svgElem: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    let rect = this.svgElem.nativeElement.getBoundingClientRect();
+    this.svgPos.x = rect.x;
+    this.svgPos.y = rect.y;
+
+  }
+
+  ngAfterViewInit() {
+    console.log(this.letters);
+  }
+
+  updateMousePosition($event) {
+    this.mousePos.x = $event.clientX - this.svgPos.x;
+    this.mousePos.y = $event.clientY - this.svgPos.y;
+
+    console.log(this.mousePos);
   }
 
 }
